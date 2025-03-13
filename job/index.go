2e51974550f1200bda488feb5a5e6fecf44eee74
ndhung <nguyenduyhung04092004@gmail.com>
2025-03-13 18:35:25 +0700

    oce

--- job/index.go
+++ job/index.go
@@ -1,20 +1,19 @@
 package jobapp
 
 import (
-	constant "app/internal/constants"
 	"log"
 
 	"github.com/robfig/cron/v3"
 )
 
 func InitJob() {
-	fileJob := NewFileJob()
+	// fileJob := NewFileJob()
 
 	c := cron.New()
 
 	log.Println("start background job")
 
-	c.AddFunc(constant.EVERY_30S, fileJob.DeleteDirEncoding)
+	// c.AddFunc(constant.EVERY_30S, fileJob.DeleteDirEncoding)
 
 	c.Start()
 
